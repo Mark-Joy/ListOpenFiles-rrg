c7b6540c4caf1cd192e2c95d13d748630fcbe18f
MarkJoy <markjoy999@gmail.com>
2024-11-13 14:51:08 +0700

    1. Support ST3 and ST4 2. Add default commands, keymap, and menu files

--- listopenfiles.py
+++ listopenfiles.py
@@ -1,14 +1,10 @@
 import sublime,sublime_plugin,os
 
 class ListOpenFilesCommand(sublime_plugin.TextCommand):
-
     def run(self, edit):
         window = sublime.active_window()
-        views = window.views()
-
         fileNames = ''
-        for view in views:
+        for view in window.views():
             if view and view.file_name():
                 fileNames += os.path.abspath(view.file_name())+'\n'
-
-        window.new_file().insert(edit, 0, "List of open files:\n\n"+fileNames)
+        window.new_file().run_command("insert",{"characters": fileNames})
